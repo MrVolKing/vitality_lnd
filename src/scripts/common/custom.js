737c55c305f42bc023ed235c4469a28858aa328e
MrVolKing <99519851vvvk@gmail.com>
2020-10-07 16:02:42 +0300

    second

--- src/scripts/common/custom.js
+++ src/scripts/common/custom.js
@@ -31,24 +31,113 @@ $(document).ready(() => {
 
 
 
-$(function(){
+// $(function(){
 
-    $("#typing").typed({
-        strings: [" В Африке гориллы.", " В Африке большие.", " Злые крокодилы."],
-        typeSpeed: 70,
-        backDelay: 1500,
-        startDelay: 2500,
-        loop: true,
-        loopCount: 2,
-        contentType: 'html',      
+//     $("#typing").typed({
+//         strings: [" В Африке гориллы.", " В Африке большие.", " Злые крокодилы."],
+//         typeSpeed: 70,
+//         backDelay: 1500,
+//         startDelay: 2500,
+//         loop: true,
+//         loopCount: 2,
+//         contentType: 'html',      
+//     });
+
+// });
+
+
+
+
+// Переключение иконок и тайтлов
+    $('#icn1').click(function() {
+        $('.topBlock__iconItem').removeClass('active');
+        $(this).addClass('active');
+        $('.topBlock__titleItemText').removeClass('active');
+        $('#title1').addClass('active');
     });
 
+    $('#icn2').click(function() {
+        $('.topBlock__iconItem').removeClass('active');
+        $(this).addClass('active');
+        $('.topBlock__titleItemText').removeClass('active');
+        $('#title2').addClass('active');
+    });
+
+    $('#icn3').click(function() {
+        $('.topBlock__iconItem').removeClass('active');
+        $(this).addClass('active');
+        $('.topBlock__titleItemText').removeClass('active');
+        $('#title3').addClass('active');
+    });
+    
+
+
+// slider
+    var swiper = new Swiper('.slider1', {
+        loop: false,
+        slidesPerView: 1,
+        centeredSlides: false,
+        // noSwiping: true,
+        breakpoints: {
+            500: {
+                slidesPerView: 1,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+            }
+        }
+    });
+
+
+    //2
+    var swiper = new Swiper('.slider2', {
+        // loop: false,
+        loop: true,
+        slidesPerView: 2,
+        centeredSlides: true,
+        // centeredSlidesBounds: true,
+        // centerInsufficientSlides: true,
+        // slidesOffsetBefore
+        // spaceBetween: 18,
+        // noSwiping: true,
+        // breakpoints: {
+        //     500: {
+        //         slidesPerView: 1,
+        //         // pagination: {
+        //         //     el: '.swiper-pagination',
+        //         //     clickable: true,
+        //         // },
+        //     }
+        // }
+    });
+
+
+
+
+});                  
+
+
+
+
+// typed
+var $text = "Що входить до складу?";
+var $count = 0;
+var $maxspeed = 1200;
+
+function character(start, end, text) {
+    return text.substring(start,end);
+}
+
+function type() {
+    var $random = Math.floor(Math.random()* $maxspeed);
+    setTimeout(type, $random);
+    $('#typing').append(character($count, $count+1, $text));
+    $count++;
+}
+window.addEventListener('scroll', function(e) {
+    var el = document.querySelector('.secondBlock');
+    if (window.scrollY > el.offsetTop) {
+        type();
+    }
 });
-});
-
-                                                                        
-
-                                                                           
-
-                                        
-
